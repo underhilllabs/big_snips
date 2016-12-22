58760551e8227b2e3acd6924479951a630b7c728
Bart Lantz <underhilllabs@gmail.com>
2016-12-21 20:59:06 -0700

    adds language

--- web/models/snippet.ex
+++ web/models/snippet.ex
@@ -4,6 +4,7 @@ defmodule BigSnips.Snippet do
   schema "snippets" do
     field :body, :string
     field :filename, :string
+    field :language, :string
     belongs_to :post, BigSnips.Post
     timestamps()
   end
@@ -13,7 +14,7 @@ defmodule BigSnips.Snippet do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:body, :filename])
-    |> validate_required([:body, :filename])
+    |> cast(params, [:body, :filename, :language])
+    |> validate_required([:body])
   end
 end
