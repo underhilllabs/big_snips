cfb452712d7fb283677936faef943ebdb1ba4e53
Bart Lantz <underhilllabs@gmail.com>
2016-12-31 10:11:32 -0700

    updates brunch config so it correctly adds phoenix js

--- brunch-config.js
+++ brunch-config.js
@@ -2,22 +2,21 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: "js/app.js",
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
-      // }
+      //joinTo: {
+      // "js/app.js": /^(web\/static\/js)/,
+      //}
       //
       // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      //order: {
+      //  before: [
+      //    "web/static/vendor/js/jquery-3.1.1.js",
+      //    "web/static/vendor/js/bootstrap.min.js"
+      //  ]
+     //}
     },
     stylesheets: {
       joinTo: "css/app.css",
